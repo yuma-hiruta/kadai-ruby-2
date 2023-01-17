f6aed58c40e686fc3108008744fc570ff2c98349
yuuma.hiruta <yuma.hiruta@fujiya-sc.co.jp>
2023-01-17 05:27:30 +0000

    Add kadai-FizzBuzz.rd

---
+++ kadai-FizzBuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num)
+  if num % 15 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
+end
+
+num_max = 100
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
